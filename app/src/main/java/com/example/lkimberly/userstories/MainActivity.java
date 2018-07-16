c849efb7d341863de9b2f70708acdb7f52c913ac
Christien Spencer Williams <willch@fb.com>
2018-07-16 14:49:37 -0700

    add home activity and nav bar to home activity to build fragments on top of

--- app/src/main/java/com/example/lkimberly/userstories/MainActivity.java
+++ app/src/main/java/com/example/lkimberly/userstories/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.lkimberly.userstories;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Uri number = Uri.parse("tel:");
+
+        Intent intent = new Intent(Intent.ACTION_SEND, number);
+        intent.putExtra(Intent.EXTRA_PHONE_NUMBER, "7049099021");
+        intent.putExtra(Intent.EXTRA_TEXT, "Hi");
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        boolean isIntentSafe = activities.size() > 0;
+
+        startActivity(Intent.createChooser(intent, getResources().getText(R.string.send_to)));
+        if (isIntentSafe){
+            startActivity(intent);
+        } else {
+            Log.d("Intent is safe", "NOT SAFE MY GUY");
+        }
+
     }
 }
