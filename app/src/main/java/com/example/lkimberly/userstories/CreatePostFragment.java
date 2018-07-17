fb2645328c9f3335b4fea255853141cd1260c8fd
Emma Tseng <emmatseng@fb.com>
2018-07-16 17:55:10 -0700

    Finished basic GUI for creating post and user profile page.

---
+++ app/src/main/java/com/example/lkimberly/userstories/CreatePostFragment.java
@@ -0,0 +1,35 @@
+package com.example.lkimberly.userstories;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+public class CreatePostFragment extends Fragment {
+
+    ImageView ivPhoto;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_create_post_fragment, container, false);
+        ivPhoto = v.findViewById(R.id.ivPhoto);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+    }
+}
---
+++ app/src/main/java/com/example/lkimberly/userstories/ProfileFragment.java
@@ -0,0 +1,23 @@
+package com.example.lkimberly.userstories;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProfileFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_profile_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+    }
+}
