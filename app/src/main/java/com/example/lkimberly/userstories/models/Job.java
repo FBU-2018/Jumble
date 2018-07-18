a02e75d581242ef7ccd57e073e29e2f461e3d3e1
Christien Spencer Williams <willch@fb.com>
2018-07-17 18:05:48 -0700

    add User, Job, and Matches objects extending parse classes

--- app/src/main/java/com/example/lkimberly/userstories/models/Job.java
+++ app/src/main/java/com/example/lkimberly/userstories/models/Job.java
@@ -1,4 +1,51 @@
 package com.example.lkimberly.userstories.models;
 
-public class Job {
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+
+@ParseClassName("Job")
+public class Job extends ParseObject {
+
+    private  static final String KEY_NAME = "user";
+
+
+
+    // Get name
+    public ParseUser getUser() {
+        return getParseUser(KEY_NAME);
+    }
+
+    public void setUser(ParseUser user) {
+        put(KEY_NAME, user);
+    }
+
+//    public ParseFile getMedia() {
+//        return getParseFile(KEY_IMAGE);
+//    }
+//
+//    public void setMedia(ParseFile parseFile) {
+//        put(KEY_IMAGE, parseFile);
+//    }
+
+    public static class Query extends ParseQuery<Job> {
+        public Query() {
+            super(Job.class);
+        }
+
+        public Query getTop() {
+            orderByDescending("createdAt");
+            setLimit(20);
+            return  this;
+        }
+
+//        public Query withUser() {
+//            include("user");
+//            return this;
+//        }
+    }
+
 }
---
+++ app/src/main/java/com/example/lkimberly/userstories/models/Matches.java
@@ -0,0 +1,72 @@
+package com.example.lkimberly.userstories.models;
+
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+
+@ParseClassName("Matches")
+public class Matches extends ParseObject {
+
+    private  static final String KEY_JOB_POSTER = "jobPoster";
+    private  static final String KEY_JOB_SUBSCRIBER = "jobSubscriber";
+    private  static final String KEY_JOB = "job";
+
+
+
+    // Get job poster
+    public ParseUser getJobPoster() {
+        return getParseUser(KEY_JOB_POSTER);
+    }
+
+    public void setJobPoster(ParseUser user) {
+        put(KEY_JOB_POSTER, user);
+    }
+
+
+    // Get job subscriber
+    public ParseUser getJobSubscriber() {
+        return getParseUser(KEY_JOB_SUBSCRIBER);
+    }
+
+    public void setJobSubscriber(ParseUser user) {
+        put(KEY_JOB_SUBSCRIBER, user);
+    }
+
+    // Get job
+    public ParseObject getJob() {
+        return getParseObject(KEY_JOB);
+    }
+
+    public void setJob(ParseObject job) {
+        put(KEY_JOB, job);
+    }
+
+
+
+    public static class Query extends ParseQuery<Matches> {
+        public Query() {
+            super(Matches.class);
+        }
+
+        public Query getTop() {
+            orderByDescending("createdAt");
+            setLimit(20);
+            return  this;
+        }
+
+        public Query withJobPoster() {
+            include(KEY_JOB_POSTER);
+            return this;
+        }
+
+        public Query withJobSubscriber() {
+            include(KEY_JOB_SUBSCRIBER);
+            return this;
+        }
+    }
+
+}
+
