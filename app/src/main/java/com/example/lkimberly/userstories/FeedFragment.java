7c8a804751add9581a0f4ca9cb1240b552827d29
Kimberly Lo <lkimberly@fb.com>
2018-07-18 14:55:57 -0700

    Populated job posting fragment with textview data

--- app/src/main/java/com/example/lkimberly/userstories/FeedFragment.java
+++ app/src/main/java/com/example/lkimberly/userstories/FeedFragment.java
@@ -7,20 +7,32 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import com.example.lkimberly.userstories.models.Job;
+import com.example.lkimberly.userstories.models.SwipeCard;
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class FeedFragment extends Fragment {
 
-    private ArrayList<String> al;
-    private ArrayAdapter<String> arrayAdapter;
-    private int i;
+    ArrayList<SwipeCard> al;
+    SwipeCardAdapter swipeCardAdapter;
+//    private ArrayAdapter<String> arrayAdapter;
+    int i;
 
     SwipeFlingAdapterView flingContainer;
+
+    ParseUser currentUser;
+//    SwipeCardAdapter swipeCardAdapter;
+    ArrayList<Job> jobs;
+//    RecyclerView rvPosts;
+//    private SwipeRefreshLayout swipeContainer;
 
     // The onCreateView method is called when Fragment should create its View object hierarchy,
     // either dynamically or via XML layout inflation.
@@ -37,29 +49,50 @@ public class FeedFragment extends Fragment {
         // Setup any handles to view objects here
         // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
 
-        al = new ArrayList<>();
-        al.add("Job One");
-        al.add("Job Two");
-        al.add("Job Three");
-        al.add("Job Four");
-        al.add("Job Five");
-        al.add("Job Six");
-        al.add("Job Seven");
-        al.add("Job Eight");
+        jobs = new ArrayList<>();
+//        swipeCardAdapter = new SwipeCardAdapter(jobs);
 
-        arrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.item, R.id.helloText, al );
+        currentUser = ParseUser.getCurrentUser();
+
+        final Job.Query postsQuery = new Job.Query();
+        postsQuery.getTop().withUser();
+
+//        al = new ArrayList<>();
+//        al.add("Job One");
+//        al.add("Job Two");
+//        al.add("Job Three");
+//        al.add("Job Four");
+//        al.add("Job Five");
+//        al.add("Job Six");
+//        al.add("Job Seven");
+//        al.add("Job Eight");
+
+//        arrayAdapter = new ArrayAdapter<>(getActivity(), R.layout.item, R.id.helloText, al );
+
+        al = new ArrayList<SwipeCard>();
+//        al.add(new SwipeCard("card1text1", "card1text2"));
+//        al.add(new SwipeCard("card2text1", "card2text2"));
+//        al.add(new SwipeCard("card3text1", "card3text2"));
+//        al.add(new SwipeCard("card4text1", "card4text2"));
+//        al.add(new SwipeCard("card5text1", "card5text2"));
+
+        loadTopPosts();
+
+        swipeCardAdapter = new SwipeCardAdapter(getContext(), getLayoutInflater(), al);
+
+//        flingContainer.setAdapter(swipeCardAdapter);
 
         flingContainer = getActivity().findViewById(R.id.frame);
 
-
-        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setAdapter(swipeCardAdapter);
         flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
             @Override
             public void removeFirstObjectInAdapter() {
                 // this is the simplest way to delete an object from the Adapter (/AdapterView)
                 Log.d("LIST", "removed object!");
-                al.remove(0);
-                arrayAdapter.notifyDataSetChanged();
+                SwipeCard temp = al.remove(0);
+                al.add(temp);
+                swipeCardAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -77,11 +110,13 @@ public class FeedFragment extends Fragment {
 
             @Override
             public void onAdapterAboutToEmpty(int itemsInAdapter) {
-                // Ask for more data here
-                al.add("XML ".concat(String.valueOf(i)));
-                arrayAdapter.notifyDataSetChanged();
+////                 Ask for more data here
+//                al.add("XML ".concat(String.valueOf(i)));
+//                swipeCardAdapter.notifyDataSetChanged();
+//                Log.d("LIST", "notified");
+//                i++;
+//                makeToast(getContext(), "No more!");
                 Log.d("LIST", "notified");
-                i++;
             }
 
             @Override
@@ -113,10 +148,51 @@ public class FeedFragment extends Fragment {
 //        public void left() {
 //            flingContainer.getTopCardListener().selectLeft();
 //        }
+
+//        rvPosts = getActivity().findViewById(R.id.rvPost);
+//        rvPosts.setLayoutManager(new LinearLayoutManager(getActivity()));
+//        rvPosts.setAdapter(postAdapter);
     }
 
     static void makeToast(Context ctx, String s){
         Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
+    }
+
+    private void loadTopPosts() {
+        final Job.Query postsQuery = new Job.Query();
+        postsQuery.getTop().withUser();
+
+        postsQuery.findInBackground(new FindCallback<Job>() {
+            @Override
+            public void done(List<Job> objects, ParseException e) {
+                if (e == null) {
+                    // posts.addAll(objects);
+                    // postAdapter.notifyDataSetChanged();
+
+                    for (int i = 0; i < objects.size(); ++i) {
+                        /*
+                        Log.d("HomeActivity", "Post[" + i + "] = "
+                                + objects.get(i).getDescription()
+                                + "\nusername = " + objects.get(i).getUser().getUsername()
+                        );
+                        */
+
+                        Job job = objects.get(objects.size() - i - 1);
+//                        jobs.add(job);
+//                        swipeCardAdapter.notifyItemInserted(jobs.size() - 1);
+                        try {
+                            al.add(new SwipeCard(job.getTitle().toString(), job.getDescription().toString()));
+                        } catch (NullPointerException e2) {
+                            al.add(new SwipeCard("EMPTY", "EMPTY"));
+                        }
+                        swipeCardAdapter.notifyDataSetChanged();
+                    }
+
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/lkimberly/userstories/SwipeCardAdapter.java
@@ -0,0 +1,205 @@
+package com.example.lkimberly.userstories;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.lkimberly.userstories.models.Job;
+import com.example.lkimberly.userstories.models.SwipeCard;
+
+import java.util.List;
+
+public class SwipeCardAdapter extends BaseAdapter {
+
+    Context context;
+    LayoutInflater inflater;
+    List<SwipeCard> mJobs;
+
+    Job job;
+
+    public SwipeCardAdapter(Context context, LayoutInflater layoutInflater, List<SwipeCard> swipeCardList) {
+        this.context = context;
+        this.inflater = layoutInflater;
+        this.mJobs = swipeCardList;
+    }
+
+//    @Override
+//    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        context = parent.getContext();
+//        inflater = LayoutInflater.from(context);
+//
+//        View postView = inflater.inflate(R.layout.item, parent, false);
+//        ViewHolder viewHolder = new ViewHolder(postView);
+//        return viewHolder;
+//    }
+
+//    public void onBindViewHolder(ViewHolder holder, int position) {
+//        job = mJobs.get(position);
+//
+//        holder.tvUsername.setText(job.getUser().getUsername());
+//        holder.tvBody.setText(job.getDescription());
+//        holder.tvTime.setText(job.getCreatedAt().toString());
+//        holder.tvTime.setText(getRelativeTimeAgo(job.getCreatedAt().toString()));
+//        holder.tvTitle.setText(job.getTitle());
+//        holder.tvDescription.setText(job.getDescription());
+//
+//        try {
+//            Glide.with(context).load(job.getImage().getUrl()).into(holder.ivJobImage);
+//        } catch (NullPointerException e) {
+//            Log.d("Post Adapter", "No Profile Pic");
+//        }
+//    }
+
+    @Override
+    public int getCount() {
+        return mJobs.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return mJobs.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        if(convertView == null){
+            convertView = inflater.inflate(R.layout.item, parent, false);
+
+            viewHolder = new ViewHolder();
+            viewHolder.textView1 = (TextView) convertView.findViewById(R.id.helloText);
+            viewHolder.textView2 = (TextView) convertView.findViewById(R.id.helloText2);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        SwipeCard sw = mJobs.get(position);
+
+        viewHolder.textView1.setText(sw.getText1());
+        viewHolder.textView2.setText(sw.getText2());
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        public TextView textView1, textView2;
+    }
+
+//    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+//
+////        public ImageView ivPostImage;
+////        public TextView tvUsername;
+////        public TextView tvBody;
+////        public TextView tvTime;
+////        public ImageView ivProfileImage;
+////        public ImageButton heartBtn;
+//        public ImageView ivJobImage;
+//        public TextView tvTitle;
+//        public TextView tvDescription;
+//
+//        public ViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//
+//            // findViewByID lookup
+////            ivPostImage = itemView.findViewById(R.id.post_image_iv);
+////            tvUsername = itemView.findViewById(R.id.post_username_et);
+////            tvBody = itemView.findViewById(R.id.post_body_et);
+////            tvTime = itemView.findViewById(R.id.post_time);
+////            ivProfileImage = itemView.findViewById(R.id.imageView3);
+////            heartBtn = itemView.findViewById(R.id.heart_btn);
+//            tvTitle = itemView.findViewById(R.id.helloText);
+//            tvDescription = itemView.findViewById(R.id.helloText2);
+//            ivJobImage = itemView.findViewById(R.id.imageView2);
+//
+//            itemView.setOnClickListener(this);
+//
+////            ivPostImage.setOnClickListener(new View.OnClickListener() {
+////                @Override
+////                public void onClick(View view) {
+////                    /*
+////                    final Intent intent = new Intent(context, DetailActivity.class);
+////                    context.startActivity(intent);
+////                    */
+////                    // gets item position
+////                    int position = getAdapterPosition();
+////                    // make sure the position is valid, i.e. actually exists in the view
+////                    if (position != RecyclerView.NO_POSITION) {
+////                        // get the movie at the position, this won't work if the class is static
+////                        Job job = mJobs.get(position);
+////                        // create intent for the new activity
+////                        Intent intent = new Intent(context, DetailActivity.class);
+////                        // serialize the movie using parceler, use its short name as a key
+////                        intent.putExtra(Job.class.getSimpleName(), job.getObjectId());
+////                        // show the activity
+////                        context.startActivity(intent);
+////                    }
+////                }
+////            });
+//        }
+//
+//        @Override
+//        public void onClick(View view) {
+////            // gets item position
+////            int position = getAdapterPosition();
+////            // make sure the position is valid, i.e. actually exists in the view
+////            if (position != RecyclerView.NO_POSITION) {
+////                // get the movie at the position, this won't work if the class is static
+////                Job job = mJobs.get(position);
+////                // create intent for the new activity
+////                Intent intent = new Intent(context, DetailActivity.class);
+////                // serialize the movie using parceler, use its short name as a key
+////                intent.putExtra(Job.class.getSimpleName(), job);
+////                // show the activity
+////                context.startActivity(intent);
+////            }
+//        }
+//    }
+
+//    // Clean all elements of the recycler
+//    public void clear() {
+//        mJobs.clear();
+//        notifyDataSetChanged();
+//    }
+//
+//    // Add a list of items -- change to type used
+//    public void addAll(List<Job> list) {
+//        mJobs.addAll(list);
+//        notifyDataSetChanged();
+//    }
+
+//    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
+//    public String getRelativeTimeAgo(String rawJsonDate) {
+//        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+//        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+//        sf.setLenient(true);
+//
+//        String relativeDate = "";
+//        try {
+//            long dateMillis = sf.parse(rawJsonDate).getTime();
+//            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+//                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+//        } catch (java.text.ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return relativeDate;
+//    }
+}
