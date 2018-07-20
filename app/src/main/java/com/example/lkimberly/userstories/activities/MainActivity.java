eb6845e9c55ba9a50b0e409859a365b8e7984daf
Kimberly Lo <lkimberly@fb.com>
2018-07-20 15:50:33 -0700

    Added location marker and zoom controls

--- app/src/main/java/com/example/lkimberly/userstories/activities/MainActivity.java
+++ app/src/main/java/com/example/lkimberly/userstories/activities/MainActivity.java
@@ -74,7 +74,8 @@ public class MainActivity extends AppCompatActivity {
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+//                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                Intent intent = new Intent(MainActivity.this, MapViewActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/lkimberly/userstories/activities/MapViewActivity.java
+++ app/src/main/java/com/example/lkimberly/userstories/activities/MapViewActivity.java
@@ -1,6 +1,9 @@
 package com.example.lkimberly.userstories.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -9,9 +12,11 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapViewActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MapViewActivity extends AppCompatActivity implements OnMapReadyCallback {
     private MapView mapView;
     private GoogleMap gmap;
 
@@ -34,7 +39,9 @@ public class MapViewActivity extends AppCompatActivity implements OnMapReadyCall
         mapView = findViewById(R.id.map_view);
         mapView.onCreate(mapViewBundle);
         mapView.getMapAsync(this);
+
     }
+
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
@@ -47,6 +54,7 @@ public class MapViewActivity extends AppCompatActivity implements OnMapReadyCall
 
         mapView.onSaveInstanceState(mapViewBundle);
     }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -64,26 +72,61 @@ public class MapViewActivity extends AppCompatActivity implements OnMapReadyCall
         super.onStop();
         mapView.onStop();
     }
+
     @Override
     protected void onPause() {
         mapView.onPause();
         super.onPause();
     }
+
     @Override
     protected void onDestroy() {
         mapView.onDestroy();
         super.onDestroy();
     }
+
     @Override
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+//        gmap = googleMap;
+//        gmap.setMinZoomPreference(12);
+//        LatLng ny = new LatLng(40.7143528, -74.0059731);
+//        gmap.moveCamera(CameraUpdateFactory.newLatLng(ny));
+
         gmap = googleMap;
         gmap.setMinZoomPreference(12);
+        gmap.setIndoorEnabled(true);
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+//                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        gmap.setMyLocationEnabled(true);
+//        gmap.getUiSettings().setMyLocationButtonEnabled(true);
+        UiSettings uiSettings = gmap.getUiSettings();
+//        uiSettings.setIndoorLevelPickerEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setMapToolbarEnabled(true);
+        uiSettings.setCompassEnabled(true);
+        uiSettings.setZoomControlsEnabled(true);
+
         LatLng ny = new LatLng(40.7143528, -74.0059731);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(ny);
+        gmap.addMarker(markerOptions);
+
         gmap.moveCamera(CameraUpdateFactory.newLatLng(ny));
     }
 }
